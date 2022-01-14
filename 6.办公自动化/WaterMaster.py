c86599405bdba73337b1c2ab794d32d7f9dfcc06
Adopat <1615581829@qq.com>
2022-01-14 16:51:27 +0800

    'update by Adopat'

--- 6.办公自动化/WaterMaster.py
+++ 6.办公自动化/WaterMaster.py
@@ -2,10 +2,10 @@
 from os.path import splitext
 from itertools import product
 from PIL import Image
-fn = '水印.jpg'
+fn = '水印2.png'
 im = Image.open(fn)
 width,height = im.size
 for pos in product(range(width),range(height)):
-    if sum(im.getpixel(pos)[:3])>460:
+    if sum(im.getpixel(pos)[:3])>564 :
         im.putpixel(pos,(255,255,255))
 im.save('无'.join(splitext(fn)))
