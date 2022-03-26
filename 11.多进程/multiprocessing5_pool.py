ae8445a1c6ba4d75d25048df0fc51aecbb23f822
Adopat <1615581829@qq.com>
2022-03-26 18:09:33 +0800

    Add Comment

--- 11.多进程/multiprocessing5_pool.py
+++ 11.多进程/multiprocessing5_pool.py
@@ -9,11 +9,14 @@ def job(x):
 
 
 def multicore():
+    # 第一种方式,创建进程池,默认使用CPU的所有核
     pool = mp.Pool(processes=2)
     res = pool.map(job, range(10))
     print(res)
+    # 第二种方式,apply_async 默认只能有一个参数
     res = pool.apply_async(job, (2,))
     print(res.get())
+    # apply_async 要实现 Pool的效果
     multi_res = [pool.apply_async(job, (i,)) for i in range(10)]
     print([res.get() for res in multi_res])
 
